9f888787c222b5812c3c3393b0b07299743ea1e1
Ethan Berg <ethan.berg@kroger.com>
2024-05-08 10:28:09 -0700

    added viewbind boilerplate in the fragment

--- app/src/main/java/com/kroger/start/ui/ComicFragment.kt
+++ app/src/main/java/com/kroger/start/ui/ComicFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.kroger.start.ComicViewModel
 import com.kroger.start.R
+import com.kroger.start.databinding.FragmentComicDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,12 +16,17 @@ class ComicFragment : Fragment() {
 
     private val comicViewModel: ComicViewModel by viewModels()
 
+    private var _binding: FragmentComicDetailsBinding? = null
+    private val binding: FragmentComicDetailsBinding
+        get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_comic_details, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentComicDetailsBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
